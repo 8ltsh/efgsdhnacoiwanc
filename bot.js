474e3ed219a8bfea27d7be42be86bf93e52339db
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-02-09 14:27:03 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1152,3 +1152,86 @@ client.on('message', message => {
         message.channel.sendEmbed(embed);
     }
 });
+
+
+const Discord = require('discord.js'),
+
+    client = new Discord.Client({sisableEveryone: true})
+
+    console.log("تم التشغيل,");
+
+client.on('guildMemberAdd', member => {
+
+const mohamed= member.guild.channels.get("533418386806407198");
+
+if(!mohamed) return;
+
+if(mohamed) {
+
+setTimeout(() => mohamed.send(`Welcome To Server Toixic`), 4000)
+
+}
+
+});
+
+const devs = ['486322208109494282' , '' , ''];
+
+const adminprefix = "@";
+
+client.on('message', message => {
+
+    var argresult = message.content.split(` `).slice(1).join(' ');
+
+      if (!devs.includes(message.author.id)) return;
+
+      
+
+  if (message.content.startsWith(adminprefix + 'ply')) {
+
+    client.user.setGame(argresult);
+
+      message.channel.sendMessage(`**  ${argresult} ply .. ✅**`)
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'wt')) {
+
+  client.user.setActivity(argresult, {type:'WATCHING'});
+
+      message.channel.sendMessage(`**  ${argresult} wt .. ✅**`)
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'ls')) {
+
+  client.user.setActivity(argresult , {type:'LISTENING'});
+
+      message.channel.sendMessage(`**  ${argresult} ls .. ✅**`)
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'st')) {
+
+    client.user.setGame(argresult, "https://www.twitch.tv/muuuuuute");
+
+      message.channel.sendMessage(`**  ${argresult} st .. ✅**`)
+
+    }
+
+  if (message.content.startsWith(adminprefix + 'us')) {
+
+  client.user.setUsername(argresult).then
+
+      message.channel.send(`**us ..**${argresult}** `)
+
+  } else
+
+  if (message.content.startsWith(adminprefix + 'av')) {
+
+  client.user.setAvatar(argresult);
+
+    message.channel.send(`**avatar ... :** `);
+
+  }
+
+  });
