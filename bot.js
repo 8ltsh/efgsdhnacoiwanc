5cb121638c13df49eec03840bdaccecdc55eb9a6
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-02-09 14:04:28 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1105,7 +1105,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    var prefix = '#'
+    var prefix = '@'
     if (message.content.startsWith(prefix + "avatar")) {
         var mentionned = message.mentions.users.first();
     var x5bzm;
@@ -1121,3 +1121,23 @@ client.on('message', message => {
       message.channel.sendEmbed(embed);
     }
 });
+
+client.on('message', message => {
+    if (message.content === "-info.u") {
+    var year = message.createdAt.getFullYear()
+    var month = message.createdAt.getMonth()
+    var day = message.createdAt.getDate()
+         let embed = new Discord.RichEmbed()
+         .setAuthor(message.author.username, message.author.avatarURL)
+      .setThumbnail(message.author.avatarURL)
+        .addField("**اسمك:**",  '**[ ' + `${message.author.username}` + ' ]**')
+          .setThumbnail(message.author.avatarURL)
+                   .setFooter(`${message.author.username}`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')
+      .addField('الكود الخاص بك:', message.author.discriminator)
+      .addField("**عدد الايام منذ افتتاح حسابك:**", message.author.createdAt.getDate())
+        .addField("** تم افتتاح حسابك عام:**", message.createdAt.getFullYear())
+            .addField("** عدد الشهور منذ افتتاح حسابك:**", message.createdAt.getMonth())
+    
+      message.channel.send({embed}); // N3k4a Is the one
+        }
+    }); // Toxic Code
