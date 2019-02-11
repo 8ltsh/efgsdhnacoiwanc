39c52b99f3c9feced14ffa322a2985c3c8b046df
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-02-11 04:21:13 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1356,12 +1356,3 @@ client.on('message', function(message) {
     client.channels.get("544486231065493514").send({embed:Dark});
     }
 });
-
-
-const bot = require('discord.js-musicbot-addon');
-
-bot.start(client, {
-  youtubeKey: "AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8",
-  musichelp : ".help",
- botPrefix : "$"
-});
