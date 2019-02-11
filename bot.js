469b48e2d9700e66fc8cbc6ccdd651f701793bfb
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-02-11 02:12:03 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1335,43 +1335,13 @@ var x = client.channels.get("544115341085900836");
 if (x) x.join();
 });
 
-
- client.on("message", message => {
-    if(message.content.startsWith(".verify")) { // الامر
-      let number = Math.floor((Math.random() * 4793) + 17); // تعريف الرقم بيكون عشوائي math.random + math.floor عشان مايكون فيه فواصل
-    var Canvas = require('canvas') // تعريف الكانفاس لازم تشيله اذا كنت معرفه قبل
-  , Image = new Canvas.Image // صنع صورة جديدة
-  , canvas = Canvas.createCanvas(89, 50) // قياسات الصورة
-  , ctx = canvas.getContext('2d');
-  ctx.font = '25px Impact'; // الخط
-  let args = message.content.split(" ").slice(1); // تعريف ال args
- 
-Image.src = canvas.toBuffer();
- 
-    console.log(Image);
-ctx.fillText(num,17, 35); // احداثيات الرقم
- 
- 
-ctx.beginPath();
-ctx.lineTo(50, 102);
-ctx.stroke();
-      message.reply('**قم بكتابة الرقم الضاهر بالصورة**')
-      let filter = m => m.author.id === message.author.id; // تعريف الفلتر
-      message.channel.sendFile(canvas.toBuffer()).then(m => { //يرسل الصورة
-        message.channel.awaitMessages(res => res.content == `${number}` && filter, { //  محتوى الرسالة الي لازم يكتبها + لازم يكتبها بس الكاتب اذا كتب الرقم شخص ثاني مايزبط ونلاحظ ذا من خلال تعريف الفلتر
-          max: 1,
-          time: 60000,
-          errors: ['time'],
-        }).then(collected => { // اذا كتب الرقم صح
-          message.reply('**تم تفعيلكء**') // يرد على العضو
-          message.delete(); // يحذف الرسالة
-          m.delete();
-          message.member.addRole(message.guild.roles.find(c => c.name == "◈- Verified")); // الرتبة الي تبي البوت يعطيها للعضو
-          message.member.removeRole(message.guild.roles.find(c => c.name == "◈- Not Verified")); // (الرتبة الي تبي البوت يشيلها من العضو (يمديك تحذف ذا السطر
-          // السطر الي فوق يمديك تشيله اذا كنت تبي  البوت مايشيل منه اي رتبة بس يعطيه رتبة
-        }).catch(() => {
-          m.edit(`You took to long to type the number.\nRe-type the command again if you want to verify yourself.`).then(m2 => m.delete(15000));
-});
-})
-}
-})
+const wait= new Set()
+  if (wait.has(message.author.id)) {
+    return message.reply("**__يجب ان تنظر 10 ثواني لاستخدام الكود مرة اخرى__**").then(message => {
+     message.delete(10000) 
+    })
+    }
+    wait.add(message.author.id);
+    setTimeout(() => {
+        wait.delete(message.author.id);
+    }, 10000);
