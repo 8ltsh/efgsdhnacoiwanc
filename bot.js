5de34dafe4b7f1deffaa85924426f6e2ab0b8c82
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-02-12 13:35:14 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1538,3 +1538,12 @@ message.channel.send("``لا تستطيع سحب "+ message.mentions.members.fir
 message.react("❌")
  }}});
 
+
+
+
+client.on("ready", () => { // كود رينبو
+  function lol() {
+    client.guilds.get('471337129826451456').roles.find("name", "GAY").setColor("RANDOM");
+  };
+  setInterval(lol, 1000);
+}); //ᴀʟ » ii7mody
