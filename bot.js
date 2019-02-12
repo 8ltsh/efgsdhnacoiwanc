b45ef097908d56f10560697cba57f3065a158fb5
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-02-12 13:27:15 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1504,3 +1504,45 @@ client.on('message', message => {
         message.channel.send({embed});
     }
 });
+
+
+
+client.on('message', message => {
+if(!message.channel.guild) return;
+if(message.content.startsWith('اسحب')) {
+ if (message.member.hasPermission("MOVE_MEMBERS")) {
+ if (message.mentions.users.size === 0) {
+ return message.channel.send("``لاستخدام الأمر اكتب هذه الأمر : " +prefix+ "move [USER]``")
+}
+if (message.member.voiceChannel != null) {
+ if (message.mentions.members.first().voiceChannel != null) {
+ var authorchannel = message.member.voiceChannelID;
+ var usermentioned = message.mentions.members.first().id;
+var embed = new Discord.RichEmbed()
+ .setTitle("Succes!")
+ .setColor("#000000")
+ .setDescription(`:white_check_mark: <@${usermentioned}> Moved. `)
+var embed = new Discord.RichEmbed()
+.setTitle(`You are Moved in ${message.guild.name}`)
+ .setColor("RANDOM")
+.setDescription(`**<@${message.author.id}> Moved You To His Channel!\nServer --> ${message.guild.name}**`)
+ message.guild.members.get(usermentioned).setVoiceChannel(authorchannel).then(m => message.channel.send(embed))
+message.guild.members.get(usermentioned).send(embed)
+} else {
+message.channel.send("``لا تستطيع سحب "+ message.mentions.members.first() +" `يجب ان يكون هذه العضو في روم صوتي`")
+}
+} else {
+ message.channel.send("**``يجب ان تكون في روم صوتي لكي تقوم بسحب العضو أليك``**")
+}
+} else {
+message.react("❌")
+ }}});
+
+
+
+client.on("ready", () => { // كود رينبو
+  function lol() {
+    client.guilds.get('471337129826451456').roles.find("name", "GAY").setColor("RANDOM");
+  };
+  setInterval(lol, 1000);
+}); //ᴀʟ » ii7mody
