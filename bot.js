8e1051a00a1ba46cb881c8887a00f1e51351e567
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-02-16 10:26:31 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1709,3 +1709,27 @@ client.on("message", message => {
         }
     }
 });
+
+
+
+lient.on('ready', () => {
+    console.log('By Hamo');
+});
+
+const sWlc = {}
+client.on('message', message => {
+var prefix = "-";
+if(message.channel.type === "dm") return;
+if(message.author.bot) return;
+  if(!sWlc[message.guild.id]) sWlc[message.guild.id] = {
+    channel: "chat"
+}
+const channel = sWlc[message.guild.id].channel
+  if (message.content.startsWith(prefix + "setwelcome")) {
+    if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
+    let newChannel = message.content.split(' ').slice(1).join(" ")
+    if(!newChannel) return message.reply(`**$setwelcome رجاء كتابت اسم الروم**`)
+    sWlc[message.guild.id].channel = newChannel
+    message.channel.send(`**${newChannel} تم عمل الترحيب في روم ${message.guild.name}**`);
+  }
+});
