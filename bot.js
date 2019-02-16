dfe28a2840b7305642f0bf9eea3b3924af6b78dd
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-02-16 09:27:12 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1633,30 +1633,19 @@ client.on('message', async message => {
 });
 
 
-client.on('message', message => {//new msg event
-if(!message.channel.guild) return;
-  if(message.content.startsWith(prefix + 'set')) {//to create the rainbow role
-      let role = message.guild.roles.find('name', 'Gay.')
-    if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
-  //start of create role
-  if(!role){
-    rainbow =  message.guild.createRole({
-   name: "Rainbow bot.",//the role will create name
-   color: "#000000",//the default color
-   permissions:[]//the permissions
- //end of create role
-})
- 
-}
-message.channel.send('Done The Rainbow Role Setup Has Been Completed')//if the step completed
-}})
- 
-client.on('ready', () => {//new ready event
-  setInterval(function(){
-      client.guilds.forEach(g => {
-                  var role = g.roles.find('name', 'Rainbow bot.');//rainbow role name
-                  if (role) {
-                      role.edit({color : "RANDOM"});
-                  };
-      });
-  }, 5000);//the rainbow time
+client.on("ready", async  => {
+setInterval(function(){
+client.channels.find('id', '544115341085900836').setName("W");
+client.channels.find('id', '544115341085900836').setName("We");
+client.channels.find('id', '544115341085900836').setName("Wel");
+client.channels.find('id', '544115341085900836').setName("Welc");
+client.channels.find('id', '544115341085900836').setName("Welco");
+client.channels.find('id', '544115341085900836').setName("Welcom");
+client.channels.find('id', '544115341085900836').setName("Welcome");
+client.channels.find('id', '544115341085900836').setName("Welcome T");
+client.channels.find('id', '544115341085900836').setName("Welcome To");
+client.channels.find('id', '544115341085900836').setName("Welcome To T");
+client.channels.find('id', '544115341085900836').setName("Welcome To D");
+client.channels.find('id', '544115341085900836').setName("Welcome To C");
+  }, 60000);
+});
