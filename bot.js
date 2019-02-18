905d5f7cafb1497d6f987d3c3d225cfdaf01a3b4
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-02-18 02:25:34 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1713,33 +1713,38 @@ client.on("message", message => {
 });
 
 
-client.on('message', async message => {
-    if (!message.guild) return;
-    if (!account[message.author.id]) {
-        account[message.author.id] = {
-            reg: false,
-            name: 'nothing'
-        };
+const afk = require('./afk.json');
+client.on('message',async rebel => {
+      if(rebel.author.bot) return;
+  if (afk[rebel.author.id]) {
+    delete afk[rebel.author.id];
+    if (rebel.member.nickname === null) {
+      msg.channel.send("أهلا بك , <@"+rebel.author.id+"> أنأ **أحاول** أن أزيل عنك الأفك.");     } else {
+      rebel.member.setNickname(rebel.member.nickname.replace(/(\[AFK\])/,''));
+      rebel.channel.send("أهلا بك , <@"+rebel.author.id+"> تم فك عنك الأفك بسبب رجوعك."); 
     }
-    if (message.content === `${prefix}register`) {
-        if (account[message.author.id].reg === true) return message.channel.send('❌ | لديك حساب مٌسجل بالفعل...');
-        if (message.author.bot) return;
-        const args = message.content.split(' ').slice(prefix.length);
-        if (!args[0]) return message.channel.send('❌ | أدخل إسم للتسجيل به.');
-        if (args[0]) {
-            account[message.author.id].reg = true;
-            account[message.author.id].name = args;
-            await saveChanges();
-            message.channel.send('تم تسجيل الحساب !!');
-        }
-    } else if (message.content === `${prefix}ping`) {
-        if (account[message.author.id].reg === false) return message.channel.send('❌ | يجب أن تكون مٌسجل لإستخدام هذا الأمر');
-        message.channel.send('PONG');
-    }
-});
-
-function saveChanges() {
-    return fs.writeFile('./account.json', JSON.stringify(account), error => {
-        if (error) console.log(error);
-    });
+    fs.writeFile("./afk.json", JSON.stringify(afk), (err) => {if (err) console.error(err);});
+} else {
+    if (rebel.content.startsWith(prefix + 'afk ')||rebel.content === prefix + 'afk') {
+      rebel.member.setNickname("[AFK] " + rebel.member.displayName);
+      let args1 = rebel.content.split(' ').slice(1);
+      if (args1.length === 0) {
+        afk[rebel.author.id] = {"reason": true}; 
+        rebel.reply("** لقد وضعتك بوضع الأفك **")
+      } else {
+        afk[rebel.author.id] = {"reason": args1.join(" ")}; // with reason
+        rebel.reply("لقد أضفتك للأفك بسبب "+ args1.join(" ") + ".")
+      }
+      fs.writeFile("./afk.json", JSON.stringify(afk), (err) => {if (err) console.error(err);});   
+  }
 }
+         var mentionned = rebel.mentions.users.first();
+if(rebel.mentions.users.size > 0) return ;
+if (afk[rebel.mentions.users.first().id]) {
+if (afk[rebel.mentions.users.first().id].reason === true) {
+rebel.channel.send(`**<@!${mentionned.id}> مأفك** `);
+}else{
+rebel.channel.send(`**<@!${mentionned.username}> مأفك , سبب الأفك \n ${afk[rebel.mentions.users.first().id].reason}**`);
+}
+} 
+});
