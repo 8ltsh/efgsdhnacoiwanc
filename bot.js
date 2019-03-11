82f7ab3dc5a668921cd05d76d77205a4a914aff0
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-03-11 02:57:23 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2058,7 +2058,7 @@ client.on("message", message => {
       }).then(collected => {
         message.delete();
         m.delete();
-        message.member.addRole(message.guild.roles.find(c => c.name == "Mēmbër's |√")); // اسم الرتبة
+        message.member.addRole(message.guild.roles.find(c => c.name == "Verify |√")); // اسم الرتبة
       }).catch(() => {
         m.edit(`لقد أخذت وقتًا طويلاً لكتابة الرقم.  قم بإعادة كتابة الأمر مرة أخرى إذا كنت تريد التحقق من هويتك..`).then(m2 => m.delete(15000));
       });
