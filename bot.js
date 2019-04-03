a48f5728842c9204ca3d63d9f4775098f89ea918
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-04-03 19:05:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1524,27 +1524,6 @@ client.on('voiceStateUpdate', (oldM, newM) => {
 
 
 
-client.on("guildMemberAdd", member => {
-  let welcome = member.guild.channels.find("name","welcome-👋");
-  if(!welcome) return;
-  if(welcome) {
-      let embed = new Discord.RichEmbed()
-      .setColor("RANDOM")
-      .setThumbnail(member.user.avatarURL)  
-      .setAuthor(member.user.username , member.user.avatarURL)
-      .addField("**Welcome To**", `[${member.guild.name}]`, true)
-      .addField(`**Number**`, `[${member.guild.memberCount}]`, true)
-      .addField("**Name**", `[${member.user.username}#${member.user.discriminator}]`,true)
-      .addField("**ID**", `[${member.user.id}]`, true)
-      .addField('**Created AT**',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')}**n** `${moment(member.user.createdAt).fromNow()}``,true)            
-      .addField("**Joined At**",`${moment(member.user.joinedAt).format('D/M/YYYY h:mm a')}**n** `${moment(member.user.joinedAt).fromNow()}``,true)    
-      welcome.send(embed)
-
-  }
-  })
-
-
-
 client.on('message', message => {
    let emoji = client.guilds.get("471337129826451456").emojis.find(r => r.name === "enigma2"); //كود تعريف المتغير emoji
  
@@ -1640,3 +1619,26 @@ client.user.setStatus('dnd');
      
       });
 
+
+
+
+
+
+client.on("guildMemberAdd", member => {
+  let welcome = member.guild.channels.find("name","welcome-👋");
+  if(!welcome) return;
+  if(welcome) {
+      let embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setThumbnail(member.user.avatarURL)  
+      .setAuthor(member.user.username , member.user.avatarURL)
+      .addField("**Welcome To**", `[${member.guild.name}]`, true)
+      .addField(`**Number**`, `[${member.guild.memberCount}]`, true)
+      .addField("**Name**", `[${member.user.username}#${member.user.discriminator}]`,true)
+      .addField("**ID**", `[${member.user.id}]`, true)
+      .addField('**Created AT**',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')}**n** `${moment(member.user.createdAt).fromNow()}``,true)            
+      .addField("**Joined At**",`${moment(member.user.joinedAt).format('D/M/YYYY h:mm a')}**n** `${moment(member.user.joinedAt).fromNow()}``,true)    
+      welcome.send(embed)
+
+  }
+  })
