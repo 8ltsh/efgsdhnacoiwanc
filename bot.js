8e358776d456bf5670261b2d71010700596977aa
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-04-04 12:44:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1623,78 +1623,11 @@ client.user.setStatus('dnd');
 
 
 
-client.on('message', message => {
+   client.on('message', message => {
     var p = message.mentions.members.first();
     var reason = message.content.split(" ").slice(2).join(' ');
-    var log = message.guild.channels.find('name', 'reportconst Discord = require('discord.js');
-const bot = new Discord.Client();
-const fs = require('fs');
-const db = require('quick.db');
- 
-// We can call the file with all the functions here.
-const func = require('./functions.js'); // If this returns an error for you (or you might be on ubuntu/linux), try '../functions.js'    
-// You can also change the name of func to something else like tools.
- 
-// We can call the JSON file here
-const commands = JSON.parse(fs.readFileSync('Storage/commands.json', 'utf8'));
-// We need to call the serverPrefixes JSON file
-const serverPrefixes = JSON.parse(fs.readFileSync('Storage/serverPrefixes.json', 'utf8'))
- 
-// Global Settings
-const prefix = '~'; // This is the prefix, you can change it to whatever you want.
- 
-// Listener Event: Runs whenever a message is received.
-bot.on('message', message => {
- 
-    // Variables - Variables make it easy to call things, since it requires less typing.
-    let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
-    let sender = message.author; // This variable takes the message, and finds who the author is.
-    let args = message.content.slice(prefix.length).trim().split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces
-    let cmd = args.shift().toLowerCase(); // This takes away the first object in the cont array, then puts it in this.
- 
-    // Message Leveling System - Make sure you require quick.db
-    db.updateValue(message.author.id + message.guild.id, 1).then(i => { // You pass it the key, which is authorID + guildID, then pass it an increase which is 1 in this instance.
-        // It also returns the new updated object, which is what we will use.
- 
-        let messages; // Create an empty variable - These IF statements will run if the new amount of messages sent is the same as the number.
-        if (i.value == 25) messages = 25; // Level 1
-        else if (i.value == 50) messages = 50; // Level 2
-        else if (i.value == 100) messages = 100; // Level 3 - You can set these to any number, and any amount of them.
- 
-        if (!isNaN(messages)) { // If messages IS STILL empty, run this.
-            db.updateValue(`userLevel_${message.author.id + message.guild.id}`, 1).then(o => { // This returns the updated object of userLevel_ID.
-                message.channel.send(`You sent ${messages} messages, so you leveled up! You are now level ${o.value}`) // Send their updated level to the channel.
-            })
-        }
- 
-    })
- 
-    // We also need to make sure it doesn't respond to bots
-    if (sender.bot) return;
-    if (!message.content.startsWith(prefix)) return; // We also want to make it so that if the message does not start with the prefix, return.
- 
-    // Command Handler - .trim() removes the blank spaces on both sides of the string
-    try {
-        let commandFile = require(`./commands/${cmd}.js`); // This will assign that filename to commandFile
-        commandFile.run(bot, message, args, func); // This will add the functions, from the functions.js file into each commandFile.
-    } catch (e) { // If an error occurs, this will run.
-        console.log(e.message); // This logs the error message
-    } finally { // This will run after the first two clear up
-        console.log(`${message.author.username} ran the command: ${cmd}`);
-    }
- 
-});
- 
-// Listener Event: Runs whenever the bot sends a ready event (when it first starts for example)
-bot.on('ready', () => {
- 
-    // We can post into the console that the bot launched.
-    console.log('Bot started.');
- 
-});
- 
-bot.login('<token>');');
-    if(message.content.startsWith(`${prefix}warn`)){
+    var log = message.guild.channels.find('name', 'report');
+    if(message.content.startsWith(`${prefix}report`)){
         if(!p) return message.reply(`**Mention the user!**`);
         if(!reason) return message.reply(`**Spofic a reason!**`);
         if(!p.bannable) return message.reply(`**I can't ban a staff member!**`);
@@ -1709,7 +1642,7 @@ bot.login('<token>');');
         reason = reason.replace('8', "**جلب اعضاء مفبركين للسيرفر**");
         reason = reason.replace('9', "**عنصريه**");
         var embed = new Discord.RichEmbed()
-        .setAuthor(`User Warned!`)
+        .setAuthor(`User reported!`)
         .addField(`Name ♣`, `<@${p.id}>`)
         .addField(`By ♣`, `<@${message.author.id}>`)
         .addField(`Reason ♣`, reason)
@@ -1725,72 +1658,245 @@ bot.login('<token>');');
 
 
 
-
-
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-const fs = require('fs');
-const db = require('quick.db');
+client.on('message', async msg => {
+    if (msg.author.bot) return undefined;
+    if (!msg.content.startsWith(prefix)) return undefined;
+   
+    const args = msg.content.split(' ');
+    const searchString = args.slice(1).join(' ');
+   
+    const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
+    const serverQueue = queue.get(msg.guild.id);
  
-// We can call the file with all the functions here.
-const func = require('./functions.js'); // If this returns an error for you (or you might be on ubuntu/linux), try '../functions.js'    
-// You can also change the name of func to something else like tools.
+    let command = msg.content.toLowerCase().split(" ")[0];
+    command = command.slice(prefix.length)
  
-// We can call the JSON file here
-const commands = JSON.parse(fs.readFileSync('Storage/commands.json', 'utf8'));
-// We need to call the serverPrefixes JSON file
-const serverPrefixes = JSON.parse(fs.readFileSync('Storage/serverPrefixes.json', 'utf8'))
+    if (command === `play`) {
+        const voiceChannel = msg.member.voiceChannel;
+       
+        if (!voiceChannel) return msg.channel.send("انت لم تدخل روم صوتي");
+       
+        const permissions = voiceChannel.permissionsFor(msg.client.user);
+       
+        if (!permissions.has('CONNECT')) {
  
-// Global Settings
-const prefix = '-'; // This is the prefix, you can change it to whatever you want.
+            return msg.channel.send("ليست لدي صلاحيات للدخول الى الروم");
+        }
+       
+        if (!permissions.has('SPEAK')) {
  
-// Listener Event: Runs whenever a message is received.
-bot.on('message', message => {
- 
-    // Variables - Variables make it easy to call things, since it requires less typing.
-    let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
-    let sender = message.author; // This variable takes the message, and finds who the author is.
-    let args = message.content.slice(prefix.length).trim().split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces
-    let cmd = args.shift().toLowerCase(); // This takes away the first object in the cont array, then puts it in this.
- 
-    // Message Leveling System - Make sure you require quick.db
-    db.updateValue(message.author.id + message.guild.id, 1).then(i => { // You pass it the key, which is authorID + guildID, then pass it an increase which is 1 in this instance.
-        // It also returns the new updated object, which is what we will use.
- 
-        let messages; // Create an empty variable - These IF statements will run if the new amount of messages sent is the same as the number.
-        if (i.value == 25) messages = 25; // Level 1
-        else if (i.value == 50) messages = 50; // Level 2
-        else if (i.value == 100) messages = 100; // Level 3 - You can set these to any number, and any amount of them.
- 
-        if (!isNaN(messages)) { // If messages IS STILL empty, run this.
-            db.updateValue(`userLevel_${message.author.id + message.guild.id}`, 1).then(o => { // This returns the updated object of userLevel_ID.
-                message.channel.send(`You sent ${messages} messages, so you leveled up! You are now level ${o.value}`) // Send their updated level to the channel.
-            })
+            return msg.channel.send("انا لا يمكنني التكلم في هاذه الروم");
         }
  
-    })
+        if (!permissions.has('EMBED_LINKS')) {
  
-    // We also need to make sure it doesn't respond to bots
-    if (sender.bot) return;
-    if (!message.content.startsWith(prefix)) return; // We also want to make it so that if the message does not start with the prefix, return.
+            return msg.channel.sendMessage("انا لا املك صلاحيات ارسال روابط")
+        }
  
-    // Command Handler - .trim() removes the blank spaces on both sides of the string
-    try {
-        let commandFile = require(`./commands/${cmd}.js`); // This will assign that filename to commandFile
-        commandFile.run(bot, message, args, func); // This will add the functions, from the functions.js file into each commandFile.
-    } catch (e) { // If an error occurs, this will run.
-        console.log(e.message); // This logs the error message
-    } finally { // This will run after the first two clear up
-        console.log(`${message.author.username} ran the command: ${cmd}`);
+        if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
+ 
+            const playlist = await youtube.getPlaylist(url);
+            const videos = await playlist.getVideos();
+           
+ 
+            for (const video of Object.values(videos)) {
+               
+                const video2 = await youtube.getVideoByID(video.id);
+                await handleVideo(video2, msg, voiceChannel, true);
+            }
+            return msg.channel.send(`**${playlist.title}**, Just added to the queue!`);
+        } else {
+ 
+            try {
+ 
+                var video = await youtube.getVideo(url);
+               
+            } catch (error) {
+                try {
+ 
+                    var videos = await youtube.searchVideos(searchString, 5);
+                    let index = 0;
+                    const embed1 = new Discord.RichEmbed()
+                    .setTitle(":mag_right:  YouTube Search Results :")
+                    .setDescription(`
+                    ${videos.map(video2 => `${++index}. **${video2.title}**`).join('\n')}`)
+                   
+                    .setColor("#f7abab")
+                    msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
+                   
+/////////////////                  
+                    try {
+ 
+                        var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
+                            maxMatches: 1,
+                            time: 15000,
+                            errors: ['time']
+                        });
+                    } catch (err) {
+                        console.error(err);
+                        return msg.channel.send('لم يتم اختيار الاغنية');
+                    }
+                   
+                    const videoIndex = parseInt(response.first().content);
+                    var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
+                   
+                } catch (err) {
+ 
+                    console.error(err);
+                    return msg.channel.send("I didn't find any results!");
+                }
+            }
+ 
+            return handleVideo(video, msg, voiceChannel);
+           
+        }
+       
+    } else if (command === `skip`) {
+ 
+        if (!msg.member.voiceChannel) return msg.channel.send("يجب ان تكون في روم صوتي");
+        if (!serverQueue) return msg.channel.send("ليست هناك اغاني ليتم التخطي");
+ 
+        serverQueue.connection.dispatcher.end('تم تخطي الاغنية');
+        return undefined;
+       
+    } else if (command === `stop`) {
+ 
+        if (!msg.member.voiceChannel) return msg.channel.send("يجب ان تكون في روم صوتي");
+        if (!serverQueue) return msg.channel.send("There is no Queue to stop!!");
+       
+        serverQueue.songs = [];
+        serverQueue.connection.dispatcher.end('تم ايقاف الاغنية لقد خرجت من الروم الصوتي');
+        return undefined;
+       
+    } else if (command === `vol`) {
+ 
+        if (!msg.member.voiceChannel) return msg.channel.send("يجب ان تكون في روم صوتي");
+        if (!serverQueue) return msg.channel.send('يعمل الامر فقط عند تشغيل مقطع صوتي');
+        if (!args[1]) return msg.channel.send(`لقد تم تغير درجة الصوت الى**${serverQueue.volume}**`);
+       
+        serverQueue.volume = args[1];
+        serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 50);
+       
+        return msg.channel.send(`درجة الصوت الان**${args[1]}**`);
+ 
+    } else if (command === `np`) {
+ 
+        if (!serverQueue) return msg.channel.send('There is no Queue!');
+        const embedNP = new Discord.RichEmbed()
+        .setDescription(`Now playing **${serverQueue.songs[0].title}**`)
+        return msg.channel.sendEmbed(embedNP);
+       
+    } else if (command === `queue`) {
+       
+        if (!serverQueue) return msg.channel.send('There is no Queue!!');
+        let index = 0;
+//  //  //
+        const embedqu = new Discord.RichEmbed()
+        .setTitle("The Queue Songs :")
+        .setDescription(`
+        ${serverQueue.songs.map(song => `${++index}. **${song.title}**`).join('\n')}
+**Now playing :** **${serverQueue.songs[0].title}**`)
+        .setColor("#f7abab")
+        return msg.channel.sendEmbed(embedqu);
+    } else if (command === `pause`) {
+        if (serverQueue && serverQueue.playing) {
+            serverQueue.playing = false;
+            serverQueue.connection.dispatcher.pause();
+            return msg.channel.send('تم الايقاف');
+        }
+        return msg.channel.send('في انتظار تشغيل المقطع');
+    } else if (command === "resume") {
+ 
+        if (serverQueue && !serverQueue.playing) {
+            serverQueue.playing = true;
+            serverQueue.connection.dispatcher.resume();
+            return msg.channel.send('تم التشغيل');
+           
+        }
+        return msg.channel.send('Queue is empty!');
     }
  
+    return undefined;
 });
  
-// Listener Event: Runs whenever the bot sends a ready event (when it first starts for example)
-bot.on('ready', () => {
+async function handleVideo(video, msg, voiceChannel, playlist = false) {
+    const serverQueue = queue.get(msg.guild.id);
+    console.log(video);
+   
  
-    // We can post into the console that the bot launched.
-    console.log('Bot started.');
+    const song = {
+        id: video.id,
+        title: Util.escapeMarkdown(video.title),
+        url: `https://www.youtube.com/watch?v=${video.id}`
+    };
+    if (!serverQueue) {
+        const queueConstruct = {
+            textChannel: msg.channel,
+            voiceChannel: voiceChannel,
+            connection: null,
+            songs: [],
+            volume: 5,
+            playing: true
+        };
+        queue.set(msg.guild.id, queueConstruct);
  
+        queueConstruct.songs.push(song);
+ 
+        try {
+            var connection = await voiceChannel.join();
+            queueConstruct.connection = connection;
+            play(msg.guild, queueConstruct.songs[0]);
+        } catch (error) {
+            console.error(`I could not join the voice channel: ${error}!`);
+            queue.delete(msg.guild.id);
+            return msg.channel.send(`Can't join this channel: ${error}!`);
+        }
+    } else {
+        serverQueue.songs.push(song);
+        console.log(serverQueue.songs);
+        if (playlist) return undefined;
+        else return msg.channel.send(`**${song.title}**, تمت اضافة المقطع الى قائمة الانتظار `);
+    }
+    return undefined;
+}
+ 
+function play(guild, song) {
+    const serverQueue = queue.get(guild.id);
+ 
+    if (!song) {
+        serverQueue.voiceChannel.leave();
+        queue.delete(guild.id);
+        return;
+    }
+    console.log(serverQueue.songs);
+ 
+    const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+        .on('end', reason => {
+            if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
+            else console.log(reason);
+            serverQueue.songs.shift();
+            play(guild, serverQueue.songs[0]);
+        })
+        .on('error', error => console.error(error));
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+ 
+    serverQueue.textChannel.send(`**${song.title}**, is now playing!`);
+}
+ 
+ 
+client.on('message', message => {
+    if (message.content === 'help') {
+        let helpEmbed = new Discord.RichEmbed()
+        .setTitle('**أوامر الميوزك...**')
+        .setDescription('**برفكس البوت (!)**')
+        .addField('play', 'لتشغيل اغنية')
+        .addField('join', 'دخول رومك الصوتي')
+        .addField('disconnect', 'الخروج من رومك الصوتي')
+        .addField('skip', 'تخطي الأغنية')
+        .addField('pause', 'ايقاف الاغنية مؤقتا')
+        .addField('resume', 'تكملة الاغنية')
+        .addField('queue', 'اظهار قائمة التشغيل')
+        .addField('np', 'اظهار الاغنية اللي انت مشغلها حاليا')
+      message.channel.send(helpEmbed);
+    }
 });
- 
