b62450667a4c03cf4b1f09bb1c610af887e1d766
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-04-06 13:12:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1658,14 +1658,11 @@ client.user.setStatus('dnd');
 
 
 
-const ytdl = require("ytdl-core");
-const { Client, Util } = require('discord.js');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
+const Discord = require('discord.js');
+const { Client, Util} = require('discord.js');
+const config = require("./config.json");
 const YouTube = require('simple-youtube-api');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const queue = new Map();
-
+const ytdl = require('ytdl-core');
 
 
 client.on('message', async msg => {
