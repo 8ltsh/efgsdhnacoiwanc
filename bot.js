f6cbd0ea2dc68107860945cf23379d668618cc96
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-04-06 15:38:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1658,11 +1658,6 @@ client.user.setStatus('dnd');
 
 
 
-const Discord = require('discord.js');
-const { Client, Util} = require('discord.js');
-const config = require("./config.json");
-const YouTube = require('simple-youtube-api');
-const ytdl = require('ytdl-core');
 
 
 client.on('message', async msg => {
@@ -1911,3 +1906,50 @@ client.on('message', message => {
 
 
 
+let vojson = JSON.parse(fs.readFileSync('vojson.json', 'utf8'))
+client.on('message', message => {
+    if(message.content.startsWith(prefix + "setVc")) {
+let channel = message.content.split(" ").slice(1).join(" ")
+let channelfind = message.guild.channels.find('name', `${channel}`)
+if(!channel) return message.channel.send('Please Type The Voice Channel Name Example: !setVc <Channel name>')
+if(!channelfind) return message.channel.send('Please Type The Voice Channel Name Example: !setVc <Channel name>')
+vojson[message.guild.id] = {
+stats: 'enable',
+chid: channelfind.id,
+guild: message.guild.id
+ 
+}
+channelfind.setName(`VoiceOnline: ${message.guild.members.filter(m => m.voiceChannel).size}`)
+message.channel.send('**Done The Voice Online  Is Turned On**')
+}
+    if(message.content.startsWith(prefix + "vc off")) {
+      message.guild.channels.find('id', `${vojson[message.guild.id].chid}`).delete()
+    vojson[message.guild.id] = {
+        stats: 'disable',
+        chid: 'undefined',
+        guild: message.guild.id
+        }
+        message.channel.send('**Done The Voice Online Is Turned Off**')
+ 
+}
+fs.writeFile("./vojson.json", JSON.stringify(vojson), (err) => {
+    if (err) console.error(err)
+  })
+})
+ 
+client.on('voiceStateUpdate', (oldMember , newMember) => {
+            if(!vojson[oldMember.guild.id]) vojson[oldMember.guild.id] = {
+                stats: 'disable',
+                chid: 'undefined',
+                guild: 'undefined'
+            }
+                    if (vojson[oldMember.guild.id].stats === 'enable') {
+                        let ch = vojson[oldMember.guild.id].chid
+                        let channel = oldMember.guild.channels.get(ch)
+                        let guildid = vojson[oldMember.guild.id].guild
+                        channel.setName(`VoiceOnline: ${oldMember.guild.members.filter(m => m.voiceChannel).size}`)
+                    };
+                    if (vojson[oldMember.guild.id].stats === 'disable') {
+                    return;
+                    }
+        });
