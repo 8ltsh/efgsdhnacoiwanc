73224e7ff8477a9144cd973ff988f50db89324fd
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-04-23 20:19:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1678,7 +1678,7 @@ client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
 :crown:${member}:crown:  
-انت العضو رقم*[ ${member.guild.memberCount} `) ]*
+انت العضو رقم ${member.guild.memberCount} `) 
 }).catch(console.error)
 })
 
