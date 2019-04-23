7f0371b3b71ac3c0ad8c7fd87a54d287ed285c9f
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-04-23 19:58:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1670,3 +1670,14 @@ client.on('message', message => {
   message.channel.sendEmbed(embed);
     }
 });
+
+
+
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
+:crown:اسم العضو  ${member}:crown:  
+انت العضو رقم ${member.guild.memberCount} `) 
+}).catch(console.error)
+})
