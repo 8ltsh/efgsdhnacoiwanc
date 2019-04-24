f92ee6807bb3cdcd3e64c57526298a5beb1820f3
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-04-24 19:18:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1673,16 +1673,3 @@ client.on("message", message => {
         }
     });
 
-
-client.on('message', message => {
-            if(!message.channel.guild) return;
-let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('send')){
- if (message.author.id !== '486322208109494282') return message.reply('** u cant wp**')
- if(!message.author.id === '486322208109494282') return;
-message.channel.sendMessage('جار ارسال الرسالة |✅')
-client.users.forEach(m =>{
-m.sendMessage(args)
-})
-}
-});
