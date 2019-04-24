954fd81f06b67227cd2ddb3e23d57c912e846f11
8ltsh <42897025+8ltsh@users.noreply.github.com>
2019-04-24 13:16:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1676,13 +1676,6 @@ client.on("message", message => {
 
 
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const fs = require('fs');
-const moment = require('moment');
-const jimp = require('jimp');
-const Canvas = require('canvas');
-
 client.on('guildMemberAdd', member => {
      const welcomer =  member.guild.channels.find('name', 'heh');
     if(!welcomer) return;
